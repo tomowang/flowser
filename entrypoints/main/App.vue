9666ee0784bb266e0330254eeb2516da2b3898cb
Tomo Wang <tomo_wang@163.com>
2026-01-13 20:51:56 +0800

    feat: Add Tailwind size indicator in dev mode

--- entrypoints/main/App.vue
+++ entrypoints/main/App.vue
@@ -7,6 +7,8 @@ import {
 import AppSidebar from "./components/AppSidebar.vue";
 import { RouterView } from "vue-router";
 import { Toaster } from "@/components/ui/sonner";
+
+const isDev = import.meta.env.DEV;
 </script>
 
 <template>
@@ -33,4 +35,15 @@ import { Toaster } from "@/components/ui/sonner";
     closeButton
     closeButtonPosition="top-right"
   />
+  <div
+    v-if="isDev"
+    class="fixed right-0 top-0 z-50 flex items-center justify-center bg-gray-200 p-2 text-sm uppercase text-black sm:bg-red-200 md:bg-yellow-200 lg:bg-green-200 xl:bg-blue-200 2xl:bg-pink-200"
+  >
+    <span class="block sm:hidden">all</span>
+    <span class="hidden sm:block md:hidden">sm</span>
+    <span class="hidden md:block lg:hidden">md</span>
+    <span class="hidden lg:block xl:hidden">lg</span>
+    <span class="hidden xl:block 2xl:hidden">xl</span>
+    <span class="hidden 2xl:block">2xl</span>
+  </div>
 </template>
